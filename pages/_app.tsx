ee79115e2e743ddb0403b941731e67cd68c73676
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2020-07-24 08:30:19 -0700

    Render SiteNav from base app instead of each page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/styles.scss'
+import SiteNav from '@components/SiteNav';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <SiteNav><Component {...pageProps} /></SiteNav>
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import Nav from '@components/SiteNav';
-
 export default function Home() {
   return (
     <div className="app">
-      <Nav></Nav>
     </div>
   )
 }
