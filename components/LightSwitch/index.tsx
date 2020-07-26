d9ec8e917ce1911f3b80fa74cf423830fb894e16
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2020-07-25 23:23:52 -0700

    Set darkMode status in browser cookie

--- components/LightSwitch/index.tsx
+++ components/LightSwitch/index.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent, useState } from "react";
 import { Lightbulb } from "@components/SVGs";
+import Cookie from "universal-cookie";
 
 interface Props {
   classes?: string
@@ -9,8 +10,15 @@ const LightSwitch: FunctionComponent<Props> = ({classes}) => {
   const [darkMode, setDarkMode] = useState(true);
   const [on, setOn] = useState(false);
 
+  const setMode = () => {
+    console.log("setting cookie");
+    const cookie = new Cookie();
+    cookie.set('darkMode', !darkMode, { path: "/"});
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div onMouseOver={() => setOn(!on)} onMouseOut={() => setOn(!on)}>
+    <div onMouseOver={() => setOn(!on)} onMouseOut={() => setOn(!on)} onClick={setMode}>
       <Lightbulb classes={classes} darkMode={darkMode} on={on}/>
     </div>
   )
