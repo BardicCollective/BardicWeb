ad258905320af64a25bda8c0d90504d11a1dec5e
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2020-08-07 23:19:05 -0700

    Convert sitenav to functional component

--- components/SiteNav/index.tsx
+++ components/SiteNav/index.tsx
@@ -1,49 +1,30 @@
-import React, { Component } from "react";
+import React, { FunctionComponent, useState } from "react";
 import styles from "./site-nav.module.scss";
 import { Close, HamburgerMenu } from "@components/SVGs";
 import LightSwitch from "@components/LightSwitch";
 
-interface SiteNavProps {}
+const SiteNav: FunctionComponent<{}> = () => {
 
-interface SiteNavStateProps {
-  collapsed: boolean;
-}
+  const [collapsed, setCollapsed] = useState(true);
 
-class SiteNav extends Component<SiteNavProps,SiteNavStateProps> {
-  constructor(props: SiteNavProps) {
-    super(props)
-    this.state = {
-      collapsed: true
-    };
+  let classes = `${styles.item} ${styles.light}`;
+  let toggle = <HamburgerMenu stroke="white" />;
+  if (!collapsed) {
+    classes += ` ${styles.active}`;
+    toggle = <Close stroke="white" />;
   }
+  return (
+    <nav className={`${styles.site_nav} ${styles.light}`}>
+      <ul className={`${styles.menu}`}>
+        <li className={`${styles.logo}`}><a className={styles.light} href='/'>Bardic Collective</a></li>
+        <li className={classes}><a className={styles.light} href='/'>Listen</a></li>
+        <li className={classes}><a className={styles.light} href='/about'>About</a></li>
 
-  handleToggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render() {
-    const { collapsed } = this.state;
-    let classes = `${styles.item} ${styles.light}`;
-    let toggle = <HamburgerMenu stroke="white" />;
-    if (!collapsed) {
-      classes += ` ${styles.active}`;
-      toggle = <Close stroke="white" />;
-    }
-    return (
-      <nav className={`${styles.site_nav} ${styles.light}`}>
-        <ul className={`${styles.menu}`}>
-          <li className={`${styles.logo}`}><a className={styles.light} href='/'>Bardic Collective</a></li>
-          <li className={classes}><a className={styles.light} href='/'>Listen</a></li>
-          <li className={classes}><a className={styles.light} href='/about'>About</a></li>
-
-          <li><LightSwitch classes={styles.lightbulb}/></li>
-          <li className={`${styles.toggle}`} onClick={this.handleToggle}>{toggle}</li>
-        </ul>
-      </nav>
-    )
-  }
+        <li><LightSwitch classes={styles.lightbulb}/></li>
+        <li className={`${styles.toggle}`} onClick={() => setCollapsed(!collapsed)}>{toggle}</li>
+      </ul>
+    </nav>
+  )
 }
 
 export default SiteNav;
